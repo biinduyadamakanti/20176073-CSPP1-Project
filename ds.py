88685362a07027827d30539cbc697b094c24a523
biinduyadamakanti <y.bindu1125@gmail.com>
2017-08-22 14:36:38 +0530

    code for 2 numbers

--- ds.py
+++ ds.py
@@ -2,10 +2,11 @@ a=open('f1.txt','r')
 b=open('f2.txt','r')
 n1=a.read()
 n2=b.read()
-for i in n1:
-	if i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-		i=i.lower()
-		print(i)
-	else:
-		i=chr(32)
+n1=n1.lower()
+
+for i in range(len(n1)):
+	if n1[i] not in 'abcdefghijklmnopqrstuvwxyz0123456789_':
+		n1=n1.replace(n1[i]," ")
 print(n1)
+
+		
