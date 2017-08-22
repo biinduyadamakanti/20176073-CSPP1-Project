a95db95181160c6a46a6ef173d96614350cc0b82
biinduyadamakanti <y.bindu1125@gmail.com>
2017-08-22 17:36:43 +0530

    optimised code 1

--- bag.py
+++ bag.py
@@ -1,9 +1,20 @@
-n1='to be or not to be' #input()
-n2='doubt truth to be a liar'#input()
+a=open("f1.txt",'r')
+b=open("f2.txt",'r')
+p1=a.read()
+p2=b.read()
+p1=p1.lower()
+p2=p2.lower()
+def filter(p):
+	s='abcdefghijklmnopqrstuvwxyz0123456789_'
+	for i in range(len(p)):
+		if p[i] not in s:
+			p=p.replace(p[i]," ")
+		return(p)
+
+n1=filter(p1)
+n2=filter(p2)
 s = n1.split(" ")
-print(s)
 p = n2.split(" ")
-print(p)
 def freq(n):
 	d={}
 	for i in n:
@@ -13,27 +24,33 @@ def freq(n):
 			d[i]=1
 	return d
 
+def dot_sum(d1,d2):
+	sum=0
+	for i in d1.keys():
+		for j in d2.keys():
+			if i==j:
+				e=d1[i]*d2[j]
+				sum=sum+e
+	return(sum)
+def sqroot(d):
+	x=0
+	for i in d:
+		x=x+((d[i])**2)
+	return(x)
+
 d1=freq(s)
 d2=freq(p)
-print(d1)
-print(d2)
-sum=0
-for i in d1.keys():
-	for j in d2.keys():
-		if i==j:
-			e=d1[i]*d2[j]
-			sum=sum+e
-print(sum)
-x=0
-y=0
+sum=dot_sum(d1,d2)
+x=sqroot(d1)
+y=sqroot(d2)
 import math
-for i in d1:
-	x=x+((d1[i])**2)
-for i in d2:
-	y=y+((d2[i])**2)
-print(x)
-print(y)
 q=(math.sqrt(x))*(math.sqrt(y))
-print(q)
 c=(sum/q)*100
 print(c)
+
+
+
+
+
+
+	
--- ds.py
+++ ds.py
@@ -1,12 +1,11 @@
-a=open('f1.txt','r')
-b=open('f2.txt','r')
-n1=a.read()
-n2=b.read()
-n1=n1.lower()
+import os
+e=os.getcwd()
+print(e)
 
-for i in range(len(n1)):
-	if n1[i] not in 'abcdefghijklmnopqrstuvwxyz0123456789_':
-		n1=n1.replace(n1[i]," ")
-print(n1)
-
-		
+e=os.chdir('C:/Users/LENOVO/Desktop/20176073_CSPP1_project/files')
+import glob
+for j in range(10):
+	i='G:/project/*.txt'
+	l=glob.glob(j)
+print(l)'''
+a=os.listdir(input("C:/Users/LENOVO/Desktop/20176073_CSPP1_project/files"))
